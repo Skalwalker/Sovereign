316b6e3bccabd10136c3046b01939e336890e8a1
Renato Avellar Nobre <rekanobre@gmail.com>
2020-09-24 23:23:58 -0300

    Updates

---
+++ precache-manifest.657c13ac70cef92660495266d66b4951.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e1a8fcd50041ce200945e8c47fe36db",
+    "url": "/Sovereign/index.html"
+  },
+  {
+    "revision": "b272be9a87b942a74d70",
+    "url": "/Sovereign/static/css/main.add3b793.chunk.css"
+  },
+  {
+    "revision": "8e9916c1ab179dcaa4dc",
+    "url": "/Sovereign/static/js/2.947a9423.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Sovereign/static/js/2.947a9423.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b272be9a87b942a74d70",
+    "url": "/Sovereign/static/js/main.e13601f8.chunk.js"
+  },
+  {
+    "revision": "2e875050797368bbf0d7",
+    "url": "/Sovereign/static/js/runtime-main.375956d1.js"
+  },
+  {
+    "revision": "e79c5cbf191e758338f018bfb3fa1f91",
+    "url": "/Sovereign/static/media/achiev.e79c5cbf.jpg"
+  },
+  {
+    "revision": "4e11096e334c83ed4276e332b0e1bffd",
+    "url": "/Sovereign/static/media/arena.4e11096e.jpg"
+  },
+  {
+    "revision": "91dd31dbcd92870f559546961ad802fd",
+    "url": "/Sovereign/static/media/bottom.91dd31db.jpg"
+  },
+  {
+    "revision": "d4e285905096248736793b8ed60199ff",
+    "url": "/Sovereign/static/media/dungeon.d4e28590.png"
+  },
+  {
+    "revision": "75ed5b31bce33ed695a81c8e702c0e69",
+    "url": "/Sovereign/static/media/logo.75ed5b31.png"
+  },
+  {
+    "revision": "5a6ce58ff10ee317d62a130c4f572ea0",
+    "url": "/Sovereign/static/media/shadowlands4k.5a6ce58f.jpg"
+  },
+  {
+    "revision": "f8d88ea8692df0ff28d99ffc20ccfbbc",
+    "url": "/Sovereign/static/media/wow.f8d88ea8.jpg"
+  }
+]);
